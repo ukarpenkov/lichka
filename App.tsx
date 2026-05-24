3459b23c2d4c2f745d0449ce2dfcf08f9ec1c22c
ukarpenkov <truloveu@gmail.com>
2026-05-24 22:02:40 +0300

    fix(app): run migrations before first render to fix white screen on cold start

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from './src/shared/config/ThemeProvider';
 import { runMigrations } from './src/shared/db';
 import { AppNavigator } from './src/app/AppNavigator';
 
-function App(): React.JSX.Element {
-  useEffect(() => {
-    runMigrations();
-  }, []);
+runMigrations();
 
+function App(): React.JSX.Element {
   return (
     <ThemeProvider>
       <AppNavigator />
